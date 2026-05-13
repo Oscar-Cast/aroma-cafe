4d34d02b33c0bc8c0408b36f523eea2b25aa2a0b
Fershos <fernandoh_lagunes@hotmail.com>
2026-05-12 18:21:59 -0600

    Modelo 3 capas y detalles seguridad
    Se creo el tercer contenedor con el frontend y se añadieron las configuraciones necesarias para la interoperabilidad, se eliminaron las contraseñas hardcodeadas ademas se eliminaron y dejaron de trackear los .env

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost', 
-  database: process.env.DB_NAME || 'aroma_cafe',
-  password: process.env.DB_PASSWORD || 'cosadelacosa', 
-  port: parseInt(process.env.DB_PORT || '5432'),
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST, 
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD, 
+  port: parseInt(process.env.DB_PORT),
 });
 
 // Verificación inicial de conexión
